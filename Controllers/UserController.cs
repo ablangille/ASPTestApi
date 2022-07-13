a93e25bb9688446bf82ce6c78bc859c7128a420d
ablangille <ablanguille@andreani.com>
2022-07-13 10:28:10 -0300

    add update request model (User), add update, get one and delete api routes (User)
    Signed-off-by: ablangille <ablanguille@andreani.com>

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TestApi.Models;
 using TestApi.Data;
 
@@ -16,33 +17,131 @@ namespace TestApi.Controllers
         }
 
         [HttpGet]
-        public IActionResult Get()
+        public async Task<IActionResult> Get()
         {
-            return Ok(dbContext.Users!.ToList());
+            return Ok(await dbContext.Users!.ToListAsync());
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> GetOne([FromRoute] Guid id)
+        {
+            var user = await dbContext.Users!.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
         }
 
         [HttpPost]
+        [Route("add")]
         public async Task<IActionResult> Add(AddUserRequest request)
         {
+            var user = new User()
+            {
+                id = Guid.NewGuid(),
+                name = request.name,
+                email = request.email,
+                dni = request.dni
+            };
+
             try
             {
-                var user = new User()
-                {
-                    id = Guid.NewGuid(),
-                    name = request.name,
-                    email = request.email,
-                    dni = request.dni
-                };
-
                 await dbContext.Users!.AddAsync(user);
                 await dbContext.SaveChangesAsync();
-
-                return Ok(user);
             }
-            catch (System.Exception e)
+            catch (Exception e)
             {
-                return BadRequest(e.Message);
+                if (
+                    e is DbUpdateException
+                    || e is DbUpdateConcurrencyException
+                    || e is OperationCanceledException
+                )
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+                }
+                else
+                {
+                    throw;
+                }
             }
+
+            return Ok(user);
+        }
+
+        [HttpPut]
+        [Route("update/{id:guid}")]
+        public async Task<IActionResult> Update([FromRoute] Guid id, UpdateUserRequest request)
+        {
+            var user = await dbContext.Users!.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.email = request.email != null ? request.email : user.email;
+            user.name = request.name != null ? request.name : user.name;
+
+            try
+            {
+                await dbContext.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                if (
+                    e is DbUpdateException
+                    || e is DbUpdateConcurrencyException
+                    || e is OperationCanceledException
+                )
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok(user);
+        }
+
+        [HttpDelete]
+        [Route("delete/{id:guid}")]
+        public async Task<IActionResult> Delete([FromRoute] Guid id)
+        {
+            var user = await dbContext.Users!.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                dbContext.Remove(user);
+                await dbContext.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                if (
+                    e is DbUpdateException
+                    || e is DbUpdateConcurrencyException
+                    || e is OperationCanceledException
+                )
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError, e.Message);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok(user);
         }
     }
 }
---
+++ Models/UpdateUserRequest.cs
@@ -0,0 +1,8 @@
+namespace TestApi.Models
+{
+    public class UpdateUserRequest
+    {
+        public string? name { get; set; }
+        public string? email { get; set; }
+    }
+}
